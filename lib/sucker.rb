2a3d95b3ac622c110b74b7550a084b2faa33b2d1
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-07-26 00:47:31 +0100

    Updated version number

--- lib/sucker.rb
+++ lib/sucker.rb
@@ -4,10 +4,8 @@ require "curb"
 require "sucker/request"
 
 module Sucker
-  VERSION             = "0.1.0".freeze
+  VERSION             = "0.2.0".freeze
   AMAZON_API_VERSION  = "2009-11-01".freeze
-
-  class SuckerError < StandardError; end
 
   def self.new(args={})
     Sucker::Request.new(args)
