d66aa0362560abb00ee3e6e3ad16bb8dabe4add9
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-07-28 11:37:32 +0100

    New spec to demo 20 items in one request

---
+++ spec/integration/twenty_items_in_one_request_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+module Sucker
+  describe "Twenty items in one request" do
+    before do
+      @worker = Sucker.new(
+        :locale => "us",
+        :key    => amazon["key"],
+        :secret => amazon["secret"])
+
+      # Prep worker
+      @worker << {
+        "Operation"                       => "ItemLookup",
+        "ItemLookup.Shared.IdType"        => "ASIN",
+        "ItemLookup.Shared.Condition"     => "All",
+        "ItemLookup.Shared.MerchantId"    => "All",
+        "ItemLookup.Shared.ResponseGroup" => "OfferFull" }
+
+      # Push twenty ASINs to worker
+      @asins = %w{
+        0816614024 0143105825 0485113600 0816616779 0942299078
+        0816614008 144006654X 0486400360 0486417670 087220474X
+        0486454398 0268018359 1604246014 184467598X 0312427182
+        1844674282 0745640974 0745646441 0826489540 1844672972 }
+      @worker << {
+        "ItemLookup.1.ItemId" => @asins[0, 10],
+        "ItemLookup.2.ItemId" => @asins[10, 10] }
+
+      @items = @worker.get["ItemLookupResponse"]["Items"].map { |items| items["Item"] }.flatten!
+    end
+
+    it "returns 20 items" do
+      @items.count.should eql 20
+      @items.map { |item| item["ASIN"] }.should eql @asins
+    end
+  end
+end
