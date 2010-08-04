4593a3e48203e521347d8654442ca26d648ae771
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-08-04 15:09:19 +0100

    Fixed gemset name in rake_rubies

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -3,8 +3,8 @@ RUBIES=$(echo `rvm list strings` | tr " " "\n")
 
 for RUBY in $RUBIES; do
   if [ $RUBY != "default" ]; then
-    rvm "$RUBY@global" ruby -S bundle install > /dev/null
-    rvm "$RUBY@global" rake $1
+    rvm "$RUBY@sucker" ruby -S bundle install > /dev/null
+    rvm "$RUBY@sucker" rake $1
   fi
 done
 
