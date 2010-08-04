5ced747de0c6c0b1ccc8d925270cb1003effa772
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-08-04 14:31:38 +0100

    Fixed rake_rubies

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -3,8 +3,7 @@ RUBIES=$(echo `rvm list strings` | tr " " "\n")
 
 for RUBY in $RUBIES; do
   if [ $RUBY != "default" ]; then
-    rvm "$RUBY@global"
-    ruby -S bundle install
-    rake $1
+    rvm "$RUBY@global" ruby -S bundle install
+    rvm "$RUBY@global" rake $1
   fi
 done
