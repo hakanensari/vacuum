baaedf6cce101d8a64b921b1f1e038649dcdf6ac
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-08-04 15:03:43 +0100

    Less verbose output when raking against rubies on system.

--- rake_rubies.sh
+++ rake_rubies.sh
@@ -3,7 +3,10 @@ RUBIES=$(echo `rvm list strings` | tr " " "\n")
 
 for RUBY in $RUBIES; do
   if [ $RUBY != "default" ]; then
-    rvm "$RUBY@global" ruby -S bundle install
+    rvm "$RUBY@global" ruby -S bundle install > /dev/null
     rvm "$RUBY@global" rake $1
   fi
 done
+
+sh ./.rvmrc > /dev/null
+bundle install > /dev/null
