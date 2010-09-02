f90993fdcc22549567d2c554c55d73b325021527
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-09-02 12:57:47 +0300

    Constants frozen

--- lib/sucker.rb
+++ lib/sucker.rb
@@ -8,8 +8,8 @@ require "sucker/response"
 # = Sucker
 # Sucker is a paper-thin Ruby wrapper to the Amazon Product Advertising API.
 module Sucker
+  AMAZON_API_VERSION = "2010-06-01".freeze
 
-  AMAZON_API_VERSION = "2010-06-01"
 
   def self.new(args={})
     Sucker::Request.new(args)
