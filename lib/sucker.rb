4650b14af3e17349ebc44e5a362d84f3733fd266
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-09-02 12:57:26 +0300

    This really should not be Sucker's concern

--- lib/sucker.rb
+++ lib/sucker.rb
@@ -8,7 +8,6 @@ require "sucker/response"
 # = Sucker
 # Sucker is a paper-thin Ruby wrapper to the Amazon Product Advertising API.
 module Sucker
-  $KCODE = "u" if RUBY_VERSION < "1.9"
 
   AMAZON_API_VERSION = "2010-06-01"
 
