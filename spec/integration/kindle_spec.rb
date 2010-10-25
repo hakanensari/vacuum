6c6b07a5f55ab36ca64370c306de7b2ebd6f1f6f
Hakan Ensari <hakan.ensari@papercavalier.com>
2010-10-25 22:52:07 +0100

    Kindle

---
+++ spec/integration/kindle_spec.rb
@@ -0,0 +1,47 @@
+require "spec_helper"
+
+module Sucker
+  describe "Kindle" do
+    context "Book index" do
+      use_vcr_cassette "integration/kindle", :record => :all
+
+      let(:items) do
+        worker = Sucker.new(
+          :locale => "us",
+          :key    => amazon["key"],
+          :secret => amazon["secret"])
+        worker << {
+          "Operation"     => "ItemSearch",
+          "SearchIndex"   => "Books",
+          "Power"         => "deleuze binding:kindle",
+          "ResponseGroup" => ["ItemAttributes"] }
+        worker.get.find("Item")
+      end
+
+      it "finds Kindle books" do
+        items.first["ItemAttributes"]["Title"].should_not be_nil
+      end
+    end
+
+    context "Kindle store index" do
+      use_vcr_cassette "integration/kindle_2", :record => :all
+
+      let(:items) do
+        worker = Sucker.new(
+          :locale => "us",
+          :key    => amazon["key"],
+          :secret => amazon["secret"])
+        worker << {
+          "Operation"     => "ItemSearch",
+          "SearchIndex"   => "KindleStore",
+          "Keywords"      => "deleuze",
+          "ResponseGroup" => ["ItemAttributes", "OfferSummary"] }
+        worker.get.find("Item")
+      end
+
+      it "finds Kindle books" do
+        items.first["ItemAttributes"]["Title"].should_not be_nil
+      end
+    end
+  end
+end
