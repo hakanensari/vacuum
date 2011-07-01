2b0787ed39c91b5b017e5c4a50c5a620d90aab5b
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-01 12:45:58 +0100

    this is shorter

--- lib/sucker/config.rb
+++ lib/sucker/config.rb
@@ -1,27 +1,7 @@
 module Sucker
   class Config
     class << self
-
-      protected
-
-      def define_configs(*configs)
-        configs.each do |config|
-          self.class.instance_eval do
-
-            define_method("#{config}=") do |value|
-              instance_variable_set("@#{config}", value)
-            end
-
-            define_method("#{config}") do
-              instance_variable_get("@#{config}")
-            end
-
-          end
-        end
-      end
+      attr_accessor :key, :secret, :associate_tag, :locale
     end
-
-    define_configs :key, :secret, :associate_tag, :locale
-
   end
 end
