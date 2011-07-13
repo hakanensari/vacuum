db64fbf80da6b5d87da2c36d69111e8128e0610d
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-13 13:14:54 +0100

    remove obsolete files

--- spec/support/amazon_credentials.rb
+++
@@ -1,7 +0,0 @@
-def amazon_key
-  ENV['AMAZON_KEY']
-end
-
-def amazon_secret
-  ENV['AMAZON_SECRET']
-end
--- spec/support/asins.rb
+++
@@ -1,8 +0,0 @@
-def asins_fixture
-  asins = File.new(File.expand_path("../../fixtures/asins.txt", __FILE__), "r").map { |line| line.chomp }
-  if block_given?
-    asins.each { |asin| yield asin }
-  end
-
-  asins
-end
