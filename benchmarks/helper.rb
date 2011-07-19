d2864615840a622790ea9a6fc017c3f9d3530f6f
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-07-19 14:53:05 +0100

    remove benchmarks

--- benchmarks/helper.rb
+++
@@ -1,6 +0,0 @@
-require "rubygems"
-require "bundler/setup"
-
-require File.expand_path("../../lib/sucker", __FILE__)
-require File.expand_path("../../spec/support/amazon_credentials", __FILE__)
-require File.expand_path("../../spec/support/asins", __FILE__)
