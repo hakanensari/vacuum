6438923c6f111132979ed136c228cd8c75c71283
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-09-05 15:19:36 +0100

    cosmetic edits

--- lib/amazon_product/builder.rb
+++ lib/amazon_product/builder.rb
@@ -2,7 +2,7 @@ module AmazonProduct
   module Builder
     # Builds a hash from a Nokogiri XML document.
     #
-    # In earlier versions of Sucker, I was relying on the XML Mini
+    # In earlier versions of the library, I was relying on the XML Mini
     # Nokogiri module in Active Support. This method essentially
     # accomplishes the same.
     #
@@ -12,33 +12,34 @@ module AmazonProduct
       when Nokogiri::XML::Document
         from_xml(xml.root)
       when Nokogiri::XML::Element
-        result_hash = {}
+        hsh = {}
 
-        xml.attributes.each_pair do |key, attribute|
-          result_hash[key] = attribute.value
+        xml.attributes.each_pair do |key, attr|
+          hsh[key] = attr.value
         end
 
         xml.children.each do |child|
           result = from_xml(child)
 
           if child.name == 'text'
-            if result_hash.empty?
+            if hsh.empty?
               return result
             else
-              result_hash['__content__'] = result
+              hsh['__content__'] = result
             end
-          elsif result_hash[child.name]
-            if result_hash[child.name].is_a? Array
-              result_hash[child.name] << result
+          elsif hsh[child.name]
+            case hsh[child.name]
+            when Array
+              hsh[child.name] << result
             else
-              result_hash[child.name] = [result_hash[child.name]] << result
+              hsh[child.name] = [hsh[child.name]] << result
             end
           else
-            result_hash[child.name] = result
+            hsh[child.name] = result
           end
         end
 
-        result_hash
+        hsh
       else
         xml.content.to_s
       end
