b32107ada800eaac368d8787f847404b7f6f9eaa
Hakan Ensari <hakan.ensari@papercavalier.com>
2011-10-26 19:50:46 +0100

    refactor specs

--- spec/amazon_product/request_spec.rb
+++ spec/amazon_product/request_spec.rb
@@ -2,257 +2,148 @@ require 'spec_helper'
 
 module AmazonProduct
   describe Request do
-    subject { Request.new('us') }
-
+    let(:req) { Request.new('us') }
 
     describe '#<<' do
       before do
-        subject.configure do |c|
+        req.configure do |c|
           c.key = 'foo'
           c.tag = 'bar'
         end
+
+        req.reset!
       end
 
       it 'merges parameters into the query' do
-        subject << { 'Key' => 'value' }
-        subject.params['Key'].should eql 'value'
+        req << { 'Key' => 'value' }
+
+        req.params['Key'].should eql 'value'
       end
 
       it 'camelizes keys' do
-        subject << { :some_key => 'value' }
-        subject.params.should have_key 'SomeKey'
+        req << { :some_key => 'value' }
+
+        req.params.should have_key 'SomeKey'
       end
 
-      it 'does not modify already-camelized keys' do
-        subject << { 'SomeKey' => 'value' }
-        subject.params.should have_key 'SomeKey'
+      it 'leaves camelized keys as is' do
+        req << { 'SomeKey' => 'value' }
+
+        req.params.should have_key 'SomeKey'
       end
 
       it 'casts numeric values to string' do
-        subject << { 'Key' => 1 }
-        subject.params['Key'].should eql '1'
+        req << { 'Key' => 1 }
+
+        req.params['Key'].should eql '1'
       end
 
       it 'converts array values to string' do
-        subject << { 'Key' => ['foo', 'bar'] }
-        subject.params['Key'].should eql 'foo,bar'
+        req << { 'Key' => ['foo', 'bar'] }
+
+        req.params['Key'].should eql 'foo,bar'
       end
 
       it 'removes whitespace after commas in values' do
-        subject << { 'Key' => 'foo,  bar' }
-        subject.params['Key'].should eql 'foo,bar'
-      end
-    end
+        req << { 'Key' => 'foo,  bar' }
 
-    describe '#aget' do
-      before do
-        subject.configure do |c|
-          c.key = 'foo'
-          c.secret = 'bar'
-          c.tag = 'baz'
-        end
-      end
-
-      after do
-        Request.adapter = :net_http
-      end
-
-      context 'when using Synchrony', :synchrony do
-        before do
-          Request.adapter = :synchrony
-        end
-
-        it 'yields a response' do
-          response = nil
-          EM.synchrony do
-            subject.aget { |resp| response = resp }
-            EM.stop
-          end
-
-          response.should be_a Response
-        end
-      end
-
-      context 'when using another adapter' do
-        it 'raises an error' do
-          expect {
-            subject.aget { }
-          }.to raise_error TypeError
-        end
+        req.params['Key'].should eql 'foo,bar'
       end
     end
 
     describe '#configure' do
       it 'yields the locale' do
-        yielded = nil
-        subject.configure do |c|
-          yielded = c
-        end
-        yielded.should be_a Locale
+        req.configure(&:class).should eql Locale
       end
     end
 
     describe '#get' do
-      shared_examples_for 'an HTTP request' do
-        before do
-          subject.configure do |c|
-            c.key = 'foo'
-            c.secret = 'bar'
-            c.tag = 'baz'
-          end
-        end
-
-        after do
-          Request.adapter = :net_http
-        end
-
-        it 'returns a response' do
-          if Request.adapter == :synchrony
-            EM.synchrony do
-              subject.get.should be_a Response
-              EM.stop
-            end
-          else
-            subject.get.should be_a Response
-          end
+      before do
+        req.configure do |c|
+          c.key    = 'foo'
+          c.secret = 'bar'
+          c.tag    = 'baz'
         end
       end
 
-      context 'when using Net::HTTP' do
-        it_behaves_like 'an HTTP request'
+      it 'returns a response' do
+        req.get.should be_a Response
       end
 
-      context 'when using Curb', :jruby do
-        before do
-          Request.adapter = :curb
-        end
+      it 'raises an error if secret is missing' do
+        req.configure { |c| c.secret = nil }
 
-        it_behaves_like 'an HTTP request'
-      end
-
-      context 'when using Synchrony', :synchrony do
-        before do
-          Request.adapter = :synchrony
-        end
-
-        it_behaves_like 'an HTTP request'
+        expect { req.get }.to raise_error MissingSecret
       end
     end
 
     describe '#params' do
-      context 'when no credentials are specified' do
-        it 'raises an error' do
-          expect do
-            subject.params
-          end.to raise_error MissingKey
-
-          expect do
-            subject.configure do |c|
-              c.key = 'foo'
-            end
-            subject.params
-          end.to raise_error MissingTag
-        end
-      end
-
-      context 'when credentials are specified' do
-        before do
-          subject.configure do |c|
-            c.key = 'foo'
-            c.tag = 'bar'
-          end
-        end
-
-        it 'returns the request parameters' do
-          subject.params['Service'].should eql 'AWSECommerceService'
-        end
-
-        it 'includes credentials' do
-          subject.params.should have_key 'AWSAccessKeyId'
-          subject.params.should have_key 'AssociateTag'
-        end
-
-        it 'includes a timestamp' do
-          subject.params.should have_key 'Timestamp'
-        end
-
-        context 'when no API version is specified' do
-          it 'includes the current API version' do
-            subject.params['Version'].should eql Request::CURRENT_API_VERSION
-          end
-        end
-
-        context 'when an API version is specified' do
-          it 'includes that API version' do
-            subject << { 'Version' => 'foo' }
-            subject.params['Version'].should eql 'foo' 
-          end
-        end
-      end
-    end
-
-    describe '#query' do
       before do
-        subject.configure do |c|
+        req.configure do |c|
           c.key = 'foo'
           c.tag = 'bar'
         end
       end
 
-      it 'canonicalizes the request parameters' do
-        subject.query.should match /\w+=\w+&/
+      it 'raises an error if key is missing' do
+        req.configure { |c| c.key = nil }
+
+        expect { req.params }.to raise_error MissingKey
       end
 
-      it 'sorts the request parameters' do
-        subject << { 'A' => 1 }
-        subject.query.should match /^A=1&/
+      it 'raises an error if tag is missing' do
+        req.configure { |c| c.tag = nil }
+
+        expect { req.params }.to raise_error MissingTag
       end
 
-      it 'URL-encodes values' do
-        subject << { :key => 'foo,bar' }
-        subject.query.should match /foo%2Cbar/
+      it 'includes common request parameters' do
+        req.params['Service'].should eql 'AWSECommerceService'
+      end
+
+      it 'includes credentials' do
+        req.params.should have_key 'AWSAccessKeyId'
+        req.params.should have_key 'AssociateTag'
+      end
+
+      it 'includes a timestamp' do
+        req.params['Timestamp'].should =~ /^\d+-\d+-\d+T\d+:\d+:\d+Z$/
+      end
+
+      context 'when no API version is specified' do
+        it 'includes the current API version' do
+          req.params['Version'].should eql Request::CURRENT_API_VERSION
+        end
+      end
+
+      context 'when an API version is specified' do
+        it 'includes the specified API version' do
+          req << { 'Version' => '1' }
+          req.params['Version'].should eql '1' 
+        end
       end
     end
 
-    describe '#reset' do
+    describe '#reset!' do
       before do
-        subject.configure do |c|
+        req.configure do |c|
           c.key = 'foo'
           c.tag = 'bar'
         end
       end
 
       it 'resets the request parameters' do
-        subject << { 'Key' => 'value' }
-        subject.params.should have_key 'Key'
+        req << { 'Key' => 'value' }
+        req.params.should have_key 'Key'
 
-        subject.reset
-        subject.params.should_not have_key 'Key'
-      end
-    end
-
-    describe '#sign' do
-      it 'adds a signature to a query' do
-        subject.configure do |c|
-          c.secret = 'baz'
-        end
-        subject.sign('foo').should match /^foo&Signature=/
-      end
-
-      it 'raises an error if no secret is specified' do
-        expect { subject.sign('foo') }.to raise_error MissingSecret
-      end
-    end
-
-    describe '#timestamp' do
-      it 'generates a timestamp' do
-        subject.timestamp.should match /^\d+-\d+-\d+T\d+:\d+:\d+Z$/
+        req.reset!
+        req.params.should_not have_key 'Key'
       end
     end
 
     describe '#url' do
       before do
-        subject.configure do |c|
+        req.configure do |c|
           c.key = 'foo'
           c.secret = 'bar'
           c.tag = 'baz'
@@ -260,7 +151,32 @@ module AmazonProduct
       end
 
       it 'builds a URL' do
-        subject.url.should be_a URI::HTTP
+        req.url.should be_a URI::HTTP
+      end
+
+      it 'canonicalizes the request parameters' do
+        req.url.query.should match /\w+=\w+&/
+      end
+
+      it 'sorts the request parameters' do
+        req << { 'A' => 1 }
+        req.url.query.should match /^A=1&/
+      end
+
+      it 'URL-encodes values' do
+        req << { :key => 'foo,bar' }
+        req.url.query.should match /foo%2Cbar/
+      end
+
+      it 'signs the query' do
+        req.url.query.should match /&Signature=/
+      end
+
+      it 'raises an error if no secret is specified' do
+        expect do
+          req.configure { |c| c.secret = nil }
+          req.url
+        end.to raise_error MissingSecret
       end
     end
   end
--- spec/amazon_product/response_spec.rb
+++ spec/amazon_product/response_spec.rb
@@ -2,92 +2,94 @@ require 'spec_helper'
 
 module AmazonProduct
   describe Response do
-    let(:response) do
+    let(:resp) do
       body = File.read(File.expand_path('../../fixtures/http_response', __FILE__))
       code = '200'
       Response.new(body, code)
     end
 
     describe '#each' do
-      context 'when a block is given' do
-        it 'yields matches to a block' do
-          yielded = false
-          response.each('Item') do |item|
-            yielded = true
-          end
-
-          yielded.should be_true
+      it 'yields matches to given block' do
+        yielded = false
+        resp.each('Item') do |item|
+          yielded = true
         end
+
+        yielded.should be_true
       end
     end
 
     describe '#errors' do
       it 'returns an array of errors' do
-        response.body =  <<-XML.gsub!(/>\s+</, '><').strip!
+        resp.body =  <<-EOF.gsub!(/>\s+</, '><').strip!
         <?xml version=\"1.0\" ?>
-        <Response xmlns="http://example.com">
+        <resp xmlns="http://example.com">
           <Errors>
             <Error>foo</Error>
           </Errors>
-        </Response>
-        XML
-        response.errors.should =~ ['foo']
+        </resp>
+        EOF
+
+        resp.errors.should =~ ['foo']
       end
     end
 
     describe '#has_errors?' do
-      context 'when a response does not contain any errors' do
+      context 'when a resp does not contain any errors' do
         it 'returns false' do
-          response.stub!(:errors).and_return([])
-          response.should_not have_errors
+          resp.stub!(:errors).and_return([])
+
+          resp.should_not have_errors
         end
       end
 
-      context 'when a response contains errors' do
+      context 'when a resp contains errors' do
         it 'returns true' do
-          response.stub!(:errors).and_return([1])
-          response.should have_errors
+          resp.stub!(:errors).and_return([1])
+
+          resp.should have_errors
         end
       end
     end
 
     describe '#find' do
       it 'returns an array of matching nodes' do
-        response.find('ASIN').should_not be_empty
+        resp.find('ASIN').should_not be_empty
       end
     end
 
     describe "#map" do
       it "yields each match to a block and maps returned values" do
-        titles = response.map('Item') { |item| item['ItemAttributes']['Title'] }
+        titles = resp.map('Item') { |i| i['ItemAttributes']['Title'] }
+
         titles.count.should eql 2
       end
     end
 
     describe '#to_hash' do
-      it 'casts response to a hash' do
-        response.to_hash.should be_a Hash
+      it 'casts resp to a hash' do
+        resp.to_hash.should be_a Hash
       end
     end
 
     describe '#valid?' do
       context 'when HTTP status is OK' do
         it 'returns true' do
-          response.should be_valid
+          resp.should be_valid
         end
       end
 
       context 'when HTTP status is not OK' do
         it 'returns false' do
-          response.code = 403
-          response.should_not be_valid
+          resp.code = 403
+          resp.should_not be_valid
         end
       end
     end
 
     describe '#xml' do
       it 'returns a Nokogiri document' do
-        response.xml.should be_an_instance_of Nokogiri::XML::Document
+        resp.xml.should be_an_instance_of Nokogiri::XML::Document
       end
     end
   end
