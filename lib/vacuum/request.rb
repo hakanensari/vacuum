f3fae325fd364deaf1c828c53c971f27cb85e2fc
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-03-13 13:49:45 +0000

    Refactor request

--- lib/vacuum/request.rb
+++ lib/vacuum/request.rb
@@ -1,5 +1,5 @@
 module Vacuum
-  # A wrapper around the request to the Amazon Product Advertising API
+  # A wrapper around the request to the Amazon Product Advertising API.
   class Request
     # The latest Amazon API version.
     CURRENT_API_VERSION = '2011-08-01'
@@ -23,7 +23,7 @@ module Vacuum
     # @option opts [#to_sym] :locale An Amazon locale
     # @option opts [String] :key An Amazon AWS access key ID
     # @option opts [String] :secret An Amazon AWS access secret key
-    # @options opts [String] :tag An Amazon Associate tag
+    # @option opts [String] :tag An Amazon Associate tag
     # @raise [MissingKey] An Amazon AWS access key ID was not given
     # @raise [MissingSecret] An Amazon AWS secret key was not given
     # @raise [MissingTag] An Amazon Associate tag was not given
@@ -68,22 +68,27 @@ module Vacuum
 
     # @return [Hash] The parameters that make up the request query.
     def params
-      { 'AWSAccessKeyId' => @key,
-        'AssociateTag'   => @tag,
-        'Service'        => 'AWSECommerceService',
-        'Timestamp'      => _timestamp,
-        'Version'        => CURRENT_API_VERSION }
-        .merge(@params)
+      _default_params.merge(@params)
     end
 
     # @return [URI::HTTP] The URL for the API request
     def url
       URI::HTTP.build :host  => @host,
                       :path  => '/onca/xml',
-                      :query => _signed_query_string
+                      :query => _query_string
     end
 
     private
+
+    def _default_params
+      default = {
+        'AWSAccessKeyId' => @key,
+        'AssociateTag'   => @tag,
+        'Service'        => 'AWSECommerceService',
+        'Timestamp'      => _timestamp,
+        'Version'        => CURRENT_API_VERSION
+      }
+    end
 
     def _escape(value)
       value.gsub(/([^a-zA-Z0-9_.~-]+)/) do
@@ -91,11 +96,7 @@ module Vacuum
       end
     end
 
-    def _reset!
-      @params = {}
-    end
-
-    def _signed_query_string
+    def _query_string
       qs   = params.sort.map { |k, v| "#{k}=" + _escape(v) }.join('&')
       dig  = OpenSSL::Digest::Digest.new 'sha256'
       req  = ['GET', @host, '/onca/xml', qs]
@@ -103,6 +104,10 @@ module Vacuum
       sig  = _escape [hmac].pack('m').chomp
 
       "#{qs}&Signature=#{sig}"
+    end
+
+    def _reset!
+      @params = {}
     end
 
     def _timestamp
