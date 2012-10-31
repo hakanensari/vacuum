87d12d356bd8aea8c71e2aba7d647ff37330bee8
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-10-31 13:41:55 +0000

    Run example in slime

--- examples/kindle_search.rb
+++ examples/kindle_search.rb
@@ -1,18 +1,13 @@
-$:.unshift File.expand_path('../../lib', __FILE__)
+require_relative 'slime'
 
-require 'pry'
 require 'yaml'
 require 'vacuum'
 
 req = Vacuum.new
-req.configure YAML.load_file File.expand_path '../amazon.yml', __FILE__
+req.configure YAML.load_file 'amazon.yml'
 
-# Also try setting SearchIndex to 'Books' and Power to
-# 'Architecture binding:kindle'.
-
+# Also try SearchIndex: 'Books' and Power: 'Architecture binding:kindle'.
 res = req.get query: { 'Operation'     => 'ItemSearch',
                        'SearchIndex'   => 'KindleStore',
                        'ResponseGroup' => 'ItemAttributes,Images',
                        'Keywords'      => 'Architecture' }
-
-binding.pry
