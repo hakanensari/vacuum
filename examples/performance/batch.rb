dca0e1dbfba5ae768b42e06cab319de977c726d5
Hakan Ensari <hakan.ensari@papercavalier.com>
2013-02-25 02:30:04 +0000

    Add back batch request example

---
+++ examples/performance/batch.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+require 'vacuum'
+
+creds = YAML.load_file 'amazon.yml'
+
+req = Vacuum.new
+req.configure creds
+
+scopes = %w(
+  ItemAttributes Images AlternateVersions
+).join(',')
+
+params = {
+  'Operation'                       => 'ItemSearch',
+  'ItemSearch.Shared.SearchIndex'   => 'Books',
+  'ItemSearch.Shared.Power'         => 'Foucault',
+  'ItemSearch.Shared.ResponseGroup' => scopes,
+  'ItemSearch.1.ItemPage'           => 1,
+  'ItemSearch.2.ItemPage'           => 2
+}
+
+res = req.get expects: 200,
+              query:   params
