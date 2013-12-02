015ef52571c32922eef2b43f1a3feba448510211
hakanensari <hakan.ensari@papercavalier.com>
2013-12-02 12:03:38 +0000

    When did Amazon stop paginating beyond page 5?

--- examples/item_search.rb
+++ examples/item_search.rb
@@ -5,9 +5,11 @@ require 'vacuum'
 req = Vacuum.new
 req.associate_tag = 'foobar'
 
+# Amazon now only paginates to page 5.
 params = {
   'SearchIndex' => 'All',
-  'Keywords'    => 'Architecture'
+  'Keywords'    => 'Architecture',
+  'ItemPage'    => 1
 }
 
 res = req.item_search(params)
