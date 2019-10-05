425d9f536b3d27cc52188d3fac6b35767fa27079
Stanislav K <lunaticman@gmail.com>
2019-10-05 14:24:55 +0700

    Validating resources

--- test/test_unit.rb
+++ test/test_unit.rb
@@ -3,6 +3,39 @@
 require 'minitest/autorun'
 require_relative '../lib/vacuum'
 
+class VacuumRequestTest < Minitest::Test
+  def test_resource_validation
+    assert_raises(ArgumentError) do
+      client.get_items(
+        item_ids: ['B07212L4G2'],
+        resources: ['WrongResource']
+      )
+    end
+
+    assert_raises(ArgumentError) do
+      client.get_items(
+        item_ids: ['B07212L4G2'],
+        resources: 'NotArrayOfResources'
+      )
+    end
+
+    assert_raises(ArgumentError) do
+      client.get_variations(
+        asin: 'B07212L4G2',
+        resources: ['WrongResource']
+      )
+    end
+  end
+
+  def client
+    Vacuum.new(
+      access_key: 'AMAZON_KEY',
+      secret_key: 'AMAZON_SECRET',
+      partner_tag: 'test-20'
+    )
+  end
+end
+
 class VacuumResponseTest < Minitest::Test
   def test_parsable_response
     assert_equal(['ItemsResult'], res.parse.keys)
@@ -27,7 +60,7 @@ class VacuumResponseTest < Minitest::Test
   BODY = %Q(
     {\"ItemsResult\":{\"Items\":[{\"ASIN\":\"B07212L4G2\",\"DetailPageURL\":\"https://www.amazon.com/dp/B07212L4G2?tag=test-20&linkCode=ogi&th=1&psc=1\",\"Images\":{\"Primary\":{\"Large\":{\"Height\":432,\"URL\":\"https://m.media-amazon.com/images/I/41OmiwuvQeL.jpg\",\"Width\":500}}},\"ItemInfo\":{\"ByLineInfo\":{\"Brand\":{\"DisplayValue\":\"adidas Originals\",\"Label\":\"Brand\",\"Locale\":\"en_US\"},\"Manufacturer\":{\"DisplayValue\":\"adidas Originals\",\"Label\":\"Manufacturer\",\"Locale\":\"en_US\"}},\"Classifications\":{\"Binding\":{\"DisplayValue\":\"T-shirt\",\"Label\":\"Binding\",\"Locale\":\"en_US\"},\"ProductGroup\":{\"DisplayValue\":\"Apparel\",\"Label\":\"ProductGroup\",\"Locale\":\"en_US\"}},\"ExternalIds\":{\"EANs\":{\"DisplayValues\":[\"0191027129406\"],\"Label\":\"EAN\",\"Locale\":\"en_US\"},\"UPCs\":{\"DisplayValues\":[\"191027129406\"],\"Label\":\"UPC\",\"Locale\":\"en_US\"}},\"Features\":{\"DisplayValues\":[\"adidas Clothing Size Guide\",\"It's like a baseball tee, but cooler!\",\"Regular fit is eased, but not sloppy, and perfect for any activity.\",\"Crew neckline.\",\"Short raglan sleeves. \\nIconic three-stripes down sleeves and a Trefoil logo at left chest. \\nStraight hemline. \\n100% cotton.\\nMachine wash, tumble dry.\\nImported.\\nMeasurements:\\n\\n     Length: 29 in\\n    \\n\\nProduct measurements were taken using size MD. Please note that measurements may vary by size.\"],\"Label\":\"Features\",\"Locale\":\"en_US\"},\"ManufactureInfo\":{\"ItemPartNumber\":{\"DisplayValue\":\"CW1207-700\",\"Label\":\"PartNumber\",\"Locale\":\"en_US\"},\"Model\":{\"DisplayValue\":\"CW1207\",\"Label\":\"Model\",\"Locale\":\"en_US\"}},\"ProductInfo\":{\"Color\":{\"DisplayValue\":\"Intense Lemons\",\"Label\":\"Color\",\"Locale\":\"en_US\"},\"IsAdultProduct\":{\"DisplayValue\":false,\"Label\":\"IsAdultProduct\",\"Locale\":\"en_US\"},\"Size\":{\"DisplayValue\":\"Medium\",\"Label\":\"Size\",\"Locale\":\"en_US\"}},\"Title\":{\"DisplayValue\":\"adidas Originals 3-Stripes Tee Intense Lemons MD\",\"Label\":\"Title\",\"Locale\":\"en_US\"}},\"Offers\":{\"Listings\":[{\"Id\":\"nTbwLoohuOnOlcqzv3tU2slwGKE%2FICZt7LOpgMtt0P6vP6Ny%2FR1RtwQ6ps130j5qKlGvZpPS0Rmtuk6y3KWCyc2v9%2B2IR0Capgos7s%2F7ahQZBCwtlpijdxRwz0c4wr8vlac3t1swYcflYBEevCPiMlajMNM2Qwra\",\"Price\":{\"Amount\":29.09,\"Currency\":\"USD\",\"DisplayAmount\":\"$29.09\"},\"ViolatesMAP\":false}]},\"ParentASIN\":\"B071K31B2L\"}]}}
   )
-  
+
   def res
     Vacuum::Response.new MockResponse.new(BODY)
   end
