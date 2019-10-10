dcafa6cd43778b7a9992933e69448e453c118004
Hakan Ensari <hakanensari@gmail.com>
2019-10-10 16:57:29 +0100

    Remove obsolete examples

--- examples/.pryrc
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-$LOAD_PATH.unshift(File.expand_path('../lib', __dir__))
-require 'vacuum'
--- examples/item_lookup.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-req = Vacuum.new
-req.associate_tag = 'foo'
-query = {
-  'ItemId' => '0912383119'
-}
-res = req.item_lookup(query: query)
-res.to_h
