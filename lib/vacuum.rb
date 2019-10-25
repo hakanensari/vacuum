fc7b272455ccdc32c8108d308e55b33b2c89070b
Hakan Ensari <hakanensari@gmail.com>
2019-10-25 21:51:27 +0100

    Generate params without a list

--- lib/vacuum.rb
+++ lib/vacuum.rb
@@ -41,13 +41,15 @@ module Vacuum
     us: Market.new('webservices.amazon.com',    'us-east-1')
   }.freeze
 
-  OPTIONAL_PARAMS = {
-    condition: 'Condition',
-    currency_of_preference: 'CurrencyOfPreference',
-    languages_of_preference: 'LanguagesOfPreference',
-    offer_count: 'OfferCount',
-    # ONLY FOR GET_VARIATIONS
-    variation_count: 'VariationCount',
-    variation_page: 'VariationPage'
-  }.freeze
+  # OPTIONAL_PARAMS = {
+  #   condition: 'Condition',
+  #   currency_of_preference: 'CurrencyOfPreference',
+  #   languages_of_preference: 'LanguagesOfPreference',
+  #   offer_count: 'OfferCount',
+  #   # ONLY FOR GET_VARIATIONS
+  #   variation_count: 'VariationCount',
+  #   variation_page: 'VariationPage',
+  #   # SEARCH
+  #   keywords: 'Keywords'
+  # }.freeze
 end
