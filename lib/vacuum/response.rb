1f629453e227a33a6e72ac110303bd2f9c1e687f
Hakan Ensari <hakanensari@gmail.com>
2019-11-13 11:59:40 +0000

    Improve YARD docs

--- lib/vacuum/response.rb
+++ lib/vacuum/response.rb
@@ -10,24 +10,29 @@ module Vacuum
     extend Forwardable
 
     # @!method dig(*key)
-    #   Delegates to {Response#to_h} to extract a nested value specified by the
-    #     sequence of keys
-    #   @param [String] key
+    #   Delegates to the Hash returned by {Response#to_h} to extract a nested
+    #   value specified by the sequence of keys
+    #   @param [String] key one or more keys
     #   @see https://ruby-doc.org/core/Hash.html#method-i-dig
     def_delegator :to_h, :dig
 
     class << self
+      # @return [nil,.parse] an optional custom parser
       attr_accessor :parser
     end
 
-    def_delegator :to_h, :dig
-
+    # @return [nil,.parse] an optional custom parser
     attr_writer :parser
 
+    # @!attribute [r] parser
+    # @return [nil,.parse] an optional custom parser
     def parser
       @parser || self.class.parser
     end
 
+    # Parses the response body
+    #
+    # @note Delegates to {#to_h} if no custom parser is set
     def parse
       parser ? parser.parse(body) : to_h
     end
