2fda8ad9fe8ae16fcd2af1fd249fa26e7be99807
Stanislav Katkov <sk@skylup.com>
2020-03-19 00:17:18 +0400

    pass rubocop and finish testing

--- test/unit/test_request.rb
+++ test/unit/test_request.rb
@@ -1,46 +1,47 @@
+# frozen_string_literal: true
+
 module Vacuum
   class TestRequest < Minitest::Test
-  	def test_search_keywords
-			err = assert_raises(ArgumentError) do
-				api.search_items(keywords: ['Harry Potter'])
-			end
+    def test_search_keywords
+      err = assert_raises(ArgumentError) do
+        api.search_items(keywords: ['Harry Potter'])
+      end
 
-			assert_equal ':keyword argument expects a String', err.message
-		end
+      assert_equal ':keyword argument expects a String', err.message
+    end
 
-		def test_search_resources
-			err = assert_raises(ArgumentError) do
-				api.search_items(
-						keywords: 'Harry Potter',
-						resources: %w(Images.Primary.Large ItemInfo.ExternalIds Offer.Listings.Price))
-			end
+    def test_search_resources
+      err = assert_raises(ArgumentError) do
+        api.search_items(
+          keywords: 'Harry Potter',
+          resources: %w[Images.Primary.Large ItemInfo.ExternalIds Offer.Listings.Price]
+        )
+      end
 
-			assert_equal 'There is not such resource: Offer.Listings.Price', err.message
-		end
+      assert_equal 'There is not such resource: Offer.Listings.Price', err.message
+    end
 
-		def test_get_items_wrong_resource
-			err = assert_raises(ArgumentError) do
-				api.get_items(
-						item_ids: 'B07212L4G2',
-						resources: %w[BrowseNodeInfo.BrowseNodes BrowseNodeInfo.BrowseNodes.Ancestor
-         BrowseNodeInfo.BrowseNode.SalesRank])
-			end
+    def test_get_items_wrong_resource
+      err = assert_raises(ArgumentError) do
+        api.get_items(
+          item_ids: :B07212L4G2,
+          resources: %w[BrowseNodeInfo.BrowseNode.SalesRank]
+        )
+      end
 
-			assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
-		end
+      assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
+    end
 
-		def test_get_variations_wrong_resource
-			err = assert_raises(ArgumentError) do
-				api.get_variations(
-						asin: 'B07212L4G2',
-						resources: %w[BrowseNodeInfo.BrowseNode.SalesRank])
-			end
+    def test_get_variations_wrong_resource
+      err = assert_raises(ArgumentError) do
+        api.get_variations(asin: 'B07212L4G2', resources: %w[BrowseNodeInfo.BrowseNode.SalesRank])
+      end
 
-			assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
-		end
+      assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
+    end
 
-		def api
-			@api ||= Vacuum.new(Locales.first)
-		end
+    def api
+      @api ||= Vacuum.new(Locales.first)
+    end
   end
 end
