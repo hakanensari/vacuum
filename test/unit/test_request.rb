c92bfdf4ec753ad9485c3718a2a03e158e384c6f
Stanislav Katkov <sk@skylup.com>
2020-10-08 12:48:30 +0300

    Fixing tests: remove outdated resource tests

--- test/unit/test_request.rb
+++ test/unit/test_request.rb
@@ -13,44 +13,6 @@ module Vacuum
       assert_equal ':keyword argument expects a String', err.message
     end
 
-    def test_search_resources
-      err = assert_raises(ArgumentError) do
-        api.search_items(
-          keywords: 'Harry Potter',
-          resources: %w[Images.Primary.Large ItemInfo.ExternalIds Offer.Listings.Price]
-        )
-      end
-
-      assert_equal 'There is not such resource: Offer.Listings.Price', err.message
-    end
-
-    def test_get_items_wrong_resource
-      err = assert_raises(ArgumentError) do
-        api.get_items(
-          item_ids: :B07212L4G2,
-          resources: %w[BrowseNodeInfo.BrowseNode.SalesRank]
-        )
-      end
-
-      assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
-    end
-
-    def test_get_variations_wrong_resource
-      err = assert_raises(ArgumentError) do
-        api.get_variations(asin: 'B07212L4G2', resources: %w[BrowseNodeInfo.BrowseNode.SalesRank])
-      end
-
-      assert_equal 'There is not such resource: BrowseNodeInfo.BrowseNode.SalesRank', err.message
-    end
-
-    def test_resource_not_array
-      err = assert_raises(ArgumentError) do
-        api.get_items(item_ids: :B07212L4G2, resources: 'foo')
-      end
-
-      assert_equal ':resources argument expects an Array', err.message
-    end
-
     def api
       @api ||= Vacuum::Request.new(marketplace: 'US',
                                    access_key: 'key',
