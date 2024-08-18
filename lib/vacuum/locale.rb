fb71faac32045f54615b6a8f83e06dc65c6ee0e0
Hakan Ensari <hakanensari@gmail.com>
2024-08-18 15:50:49 +0200

    Add Belgium

--- lib/vacuum/locale.rb
+++ lib/vacuum/locale.rb
@@ -11,8 +11,10 @@ module Vacuum
 
     HOSTS_AND_REGIONS = {
       au: ['webservices.amazon.com.au', 'us-west-2'],
+      be: ['webservices.amazon.be', 'eu-west-1'],
       br: ['webservices.amazon.com.br', 'us-east-1'],
       ca: ['webservices.amazon.ca', 'us-east-1'],
+      eg: ['webservices.amazon.eg', 'eu-west-1'],
       fr: ['webservices.amazon.fr', 'eu-west-1'],
       de: ['webservices.amazon.de', 'eu-west-1'],
       in: ['webservices.amazon.in', 'eu-west-1'],
@@ -20,16 +22,15 @@ module Vacuum
       jp: ['webservices.amazon.co.jp', 'us-west-2'],
       mx: ['webservices.amazon.com.mx', 'us-east-1'],
       nl: ['webservices.amazon.nl', 'eu-west-1'],
+      pl: ['webservices.amazon.pl', 'eu-west-1'],
       sg: ['webservices.amazon.sg', 'us-west-2'],
+      sa: ['webservices.amazon.sa', 'eu-west-1'],
       es: ['webservices.amazon.es', 'eu-west-1'],
+      se: ['webservices.amazon.se', 'eu-west-1'],
       tr: ['webservices.amazon.com.tr', 'eu-west-1'],
       ae: ['webservices.amazon.ae', 'eu-west-1'],
       gb: ['webservices.amazon.co.uk', 'eu-west-1'],
-      us: ['webservices.amazon.com', 'us-east-1'],
-      eg: ['webservices.amazon.eg', 'eu-west-1'],
-      pl: ['webservices.amazon.pl', 'eu-west-1'],
-      sa: ['webservices.amazon.sa', 'eu-west-1'],
-      se: ['webservices.amazon.se', 'eu-west-1']
+      us: ['webservices.amazon.com', 'us-east-1']
     }.freeze
     private_constant :HOSTS_AND_REGIONS
 
